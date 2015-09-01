1573d5d26afdbe97423f726c1d2284ab0cd72b92
Martin Hebnes Pedersen <martinhpedersen@users.noreply.github.com>
2015-09-01 17:56:47 +0200

    Add timestamp related tests.

--- fap_test.go
+++ fap_test.go
@@ -2,6 +2,8 @@ package fap_test
 
 import (
 	"fmt"
+	"testing"
+	"time"
 
 	"github.com/martinhpedersen/libfap-go"
 )
@@ -16,4 +18,36 @@ func ExampleParseAprs() {
 	//Speed: 50km/h
 	//Comment: ]WLNK-1=
 	//Mic-E: in service
+}
+
+func TestTimestamp(t *testing.T) {
+	// The APRS timestamp is parsed relative to runtime date, so we construct a packet with a timestamp for today.
+	rawPacket := fmt.Sprintf("LA5NTA-9>V0QRR9,LD5BE*,WIDE2-2,qAR,LA2VSA-1:/%02d2045z4903.50N/07201.75W>Test1234", time.Now().Day())
+
+	packet, _ := fap.ParseAprs(rawPacket, false)
+	if packet.RawTimestamp == "" {
+		t.Error("Expected non-empty raw timestamp.")
+	}
+
+	if packet.Timestamp.IsZero() {
+		t.Error("Expected non-zero timestamp.")
+	}
+
+	packet.Timestamp = packet.Timestamp.UTC()
+
+	if h := packet.Timestamp.Hour(); h != 20 {
+		t.Errorf("Unexpected timestamp hour: %d.", h)
+	}
+
+	if m := packet.Timestamp.Minute(); m != 45 {
+		t.Errorf("Unexpected timestamp minute: %d.", m)
+	}
+
+	if time.Now().UTC().Month() != packet.Timestamp.Month() {
+		t.Errorf("Unexpected month, got '%s' expected '%s'.", packet.Timestamp.Month(), time.Now().UTC().Month())
+	}
+
+	if time.Now().UTC().Year() != packet.Timestamp.Year() {
+		t.Errorf("Unexpected year, got '%d' expected '%d'.", packet.Timestamp.Year(), time.Now().UTC().Year())
+	}
 }
